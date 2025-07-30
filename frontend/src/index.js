3697d12bbdf325bc1524e057fd9f33d43c14089a
htailor0121 <htailor0121@gamil.com>
2025-07-30 16:59:34 +0530

    new3

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './pages/App'; // Assuming App is inside src/pages
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
