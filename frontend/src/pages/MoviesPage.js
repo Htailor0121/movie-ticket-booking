21fe42fc5a4dacbe50f77212e56107978c092d53
htailor0121 <htailor0121@gamil.com>
2025-07-31 10:48:24 +0530

    changed moviesPage.js

--- frontend/src/pages/MoviesPage.js
+++ frontend/src/pages/MoviesPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { movies } from './moviesData'; // Ensure this matches the file name
+import movies from './moviesData'; // ✅ CORRECT
 import './MoviesPage.css';
 
 const MoviesPage = () => {
