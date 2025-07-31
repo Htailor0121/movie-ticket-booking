8dae4c22b93c5f0abd54454a5fa7aadfec0a8545
htailor0121 <htailor0121@gamil.com>
2025-07-31 10:43:37 +0530

    added moviesData.js

---
+++ frontend/src/pages/moviesData.js
@@ -0,0 +1,95 @@
+const moviesData = [
+    {
+      id: 1,
+      title: 'Inception',
+      description: 'A mind-bending thriller by Christopher Nolan.',
+      image: 'https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SY679_.jpg',
+    },
+    {
+      id: 2,
+      title: 'The Matrix',
+      description: 'A hacker discovers the real world beyond the simulation.',
+      image: 'https://m.media-amazon.com/images/I/51EG732BV3L.jpg',
+    },
+    {
+      id: 3,
+      title: 'Interstellar',
+      description: 'A team travels through a wormhole to save humanity.',
+      image: 'https://m.media-amazon.com/images/I/71n58tmWz5L._AC_SY679_.jpg',
+    },
+    {
+      id: 4,
+      title: 'The Dark Knight',
+      description: 'Batman faces the Joker in a battle for Gotham.',
+      image: 'https://m.media-amazon.com/images/I/51k0qaipZfL.jpg',
+    },
+    {
+      id: 5,
+      title: 'Avatar',
+      description: 'A marine becomes part of the Na\'vi on Pandora.',
+      image: 'https://m.media-amazon.com/images/I/41kTVLeW1CL.jpg',
+    },
+    {
+      id: 6,
+      title: 'Titanic',
+      description: 'A tragic love story aboard the ill-fated Titanic.',
+      image: 'https://m.media-amazon.com/images/I/51rggtPgmRL.jpg',
+    },
+    {
+      id: 7,
+      title: 'Avengers: Endgame',
+      description: 'The Avengers fight to reverse Thanos\' snap.',
+      image: 'https://m.media-amazon.com/images/I/81ExhpBEbHL._AC_SY679_.jpg',
+    },
+    {
+      id: 8,
+      title: 'Joker',
+      description: 'A failed comedian descends into madness.',
+      image: 'https://m.media-amazon.com/images/I/71o5QG2cx7L._AC_SY679_.jpg',
+    },
+    {
+      id: 9,
+      title: 'Spider-Man: No Way Home',
+      description: 'Spider-Men unite across the multiverse.',
+      image: 'https://m.media-amazon.com/images/I/71oRM1HVdzL._AC_SY679_.jpg',
+    },
+    {
+      id: 10,
+      title: 'Doctor Strange',
+      description: 'A surgeon becomes the master of the mystic arts.',
+      image: 'https://m.media-amazon.com/images/I/71niXI3lxlL._AC_SY679_.jpg',
+    },
+    {
+      id: 11,
+      title: 'Black Panther',
+      description: 'T\'Challa returns to Wakanda to take the throne.',
+      image: 'https://m.media-amazon.com/images/I/71sFgrtQ5yL._AC_SY679_.jpg',
+    },
+    {
+      id: 12,
+      title: 'Iron Man',
+      description: 'Tony Stark builds a suit to escape captivity.',
+      image: 'https://m.media-amazon.com/images/I/81l+9kK8iPL._AC_SY679_.jpg',
+    },
+    {
+      id: 13,
+      title: 'Captain America: Civil War',
+      description: 'The Avengers are divided over government control.',
+      image: 'https://m.media-amazon.com/images/I/91bXf1qN0vL._AC_SY679_.jpg',
+    },
+    {
+      id: 14,
+      title: 'Guardians of the Galaxy',
+      description: 'A group of misfits band together to save the galaxy.',
+      image: 'https://m.media-amazon.com/images/I/71N3AcwQk8L._AC_SY679_.jpg',
+    },
+    {
+      id: 15,
+      title: 'Thor: Ragnarok',
+      description: 'Thor teams up with Hulk to stop Hela.',
+      image: 'https://m.media-amazon.com/images/I/81r+LNJzszL._AC_SY679_.jpg',
+    },
+  ];
+  
+  export default moviesData;
+  
